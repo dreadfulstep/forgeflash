25ba044e48752b9e58d85ebbba95a6628513ad9b
iazxgjas <dylanmcnamee054@gmail.com>
2023-12-22 18:58:48 +0000

    commit

---
+++ js/game.js
@@ -0,0 +1,45 @@
+const cdn = "http://127.0.0.1:3000";
+const gamesContainer = document.querySelector('.games');
+const gameContainer = document.querySelector('.gamecontainer');
+
+fetch('../assets/games.json')
+  .then((res) => res.json())
+  .then((games) => {
+    // Loop through each game and create a new game element for it
+    games.forEach((game) => {
+      const gameEl = document.createElement('div');
+      gameEl.className = 'game';
+      gameEl.innerHTML = `<a href="${cdn + "/games/" + game.root + "/" + game.file}"><img src="${cdn + "/" + game.root + "/" + game.img}" onerror="this.src='./assets/globe.svg'"/><span>${game.name}</span></a>`;
+      gamesContainer.appendChild(gameEl);
+}).catch((e) => {
+  alert('Could not load games');
+  alert(e);
+})})
+
+var button = document.querySelector(".browsegms");
+button.addEventListener("click", function(event) {
+  event.preventDefault();
+  var games = document.getElementById("games");
+  games.scrollIntoView({behavior: "smooth"});
+})
+
+function filterGames() {
+  let input = document.getElementById("search").value.toLowerCase();
+  let games = document.getElementsByClassName("game");
+  let nogames = document.getElementsByClassName("nogames")[0];
+  let count = 0;
+  for (let i = 0; i < games.length; i++) {
+    let title = games[i].getElementsByTagName("span")[0].innerHTML.toLowerCase();
+    if (title.includes(input)) {
+      games[i].style.display = "block";
+      count++;
+    } else {
+      games[i].style.display = "none";
+    }
+  }
+  if (count == 0) {
+    nogames.style.display = "block";
+  } else {
+    nogames.style.display = "none";
+  }
+}
