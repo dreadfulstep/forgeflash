458bf57796e1438b1fd6ae5b6e79201dca3bcb61
dreadfulstep <dreadfulstep@gmail.com>
2024-01-30 17:44:24 +0000

    commit

--- js/name.js
+++ js/name.js
@@ -1,3 +1,298 @@
+const itemsContainer = document.querySelector('.items');
+const thing = document.querySelector('.thing');
+const thing2 = document.querySelector('.pagegms');
+const pagegmsiframe = document.getElementById('pagegmsiframe');
+const fullscreen = document.getElementById('pagegmsclose');
+
+fetch('./assets/name.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name1.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name2.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name3.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name4.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name5.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name6.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name7.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name8.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name10.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name11.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name12.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name13.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name14.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name15.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name16.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
+fetch('./assets/name17.json')
+        .then((res) => res.json())
+        .then((items) => {
+            items.forEach((item) => {
+            if (item.name === "Request") {
+                const itemEl = document.createElement('div');
+                itemEl.className = 'item';
+                itemEl.innerHTML = `<a href="/contact"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+                itemsContainer.appendChild(itemEl);
+                return;
+            }
+            const itemEl = document.createElement('div');
+            itemEl.className = 'item';
+            itemEl.innerHTML = `<a href="./math/${item.root}/${item.file}"><img src="${item.img}" onerror="this.src='./assets/globe.svg'"/><span>${item.name}</span></a>`;
+            itemsContainer.appendChild(itemEl);
+})});
+
 var button = document.querySelector(".browsegms");
 button.addEventListener("click", function(event) {
   event.preventDefault();
